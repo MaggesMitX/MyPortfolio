e88133dbeb42a926bc6998e93eb1505a0b927152
00Shinigami <mail@max-ramacher.de>
2022-06-20 15:48:22 +0200

    adding second pin control GPIO21

--- Scripts-on-work/RGB-Project/Kampf_LED_Tester/ledStressTester.py
+++ Scripts-on-work/RGB-Project/Kampf_LED_Tester/ledStressTester.py
@@ -11,11 +11,11 @@ import argparse
 
 # LED strip configuration:
 LED_COUNT = 250       # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_PIN = 21          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN_2 = 12        # GPIO pin connected to the pixels (12 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 180  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -62,26 +62,54 @@ def rainbow(strip, wait_ms=20, iterations=1):
         time.sleep(wait_ms / 1000.0)
 
 
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-    """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256 * iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel(
-                (int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
+# def rainbowCycle(strip, wait_ms=20, iterations=5):
+#     """Draw rainbow that uniformly distributes itself across all pixels."""
+#     for j in range(256 * iterations):
+#         for i in range(strip.numPixels()):
+#             strip.setPixelColor(i, wheel(
+#                 (int(i * 256 / strip.numPixels()) + j) & 255))
+#         strip.show()
+#         time.sleep(wait_ms / 1000.0)
 
 
-def theaterChaseRainbow(strip, wait_ms=50):
-    """Rainbow movie theater light style chaser animation."""
-    for j in range(256):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, wheel((i + j) % 255))
+# def theaterChaseRainbow(strip, wait_ms=50):
+#     """Rainbow movie theater light style chaser animation."""
+#     for j in range(256):
+#         for q in range(3):
+#             for i in range(0, strip.numPixels(), 3):
+#                 strip.setPixelColor(i + q, wheel((i + j) % 255))
+#             strip.show()
+#             time.sleep(wait_ms / 1000.0)
+#             for i in range(0, strip.numPixels(), 3):
+#                 strip.setPixelColor(i + q, 0)
+
+def strobews1(strip, wait_ms=1000, strobe_count=1, pulse_count=1):
+    from random import randrange
+    """LED als springender Ball"""
+    for strobe in range(strobe_count):    
+        for pulse in range(pulse_count):
+            for i in range(strip.numPixels()):
+                strip.setPixelColorRGB(i, 0,255,0)
             strip.show()
-            time.sleep(wait_ms / 1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, 0)
+            time.sleep(randrange(0,45,1)/1000.0)
+            for i in range(strip.numPixels()):
+                strip.setPixelColorRGB(i, 0,0,0)
+            strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def strobews2(strip2, wait_ms=1000, strobe_count=2, pulse_count=1):
+    from random import randrange
+    """LED als springender Ball"""
+    for strobe in range(strobe_count):    
+        for pulse in range(pulse_count):
+            for i in range(strip2.numPixels()):
+                strip2.setPixelColorRGB(i, 0,255,0)
+            strip2.show()
+            time.sleep(randrange(0,45,1)/1000.0)
+            for i in range(strip2.numPixels()):
+                strip2.setPixelColorRGB(i, 0,0,0)
+            strip2.show()
+        time.sleep(wait_ms/1000.0)                        
 
 
 # Main program logic follows:
@@ -93,8 +121,11 @@ if __name__ == '__main__':
 
     # Create NeoPixel object with appropriate configuration.
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    #Create NeoPixel object for second pin
+    strip2 = PixelStrip(LED_COUNT, LED_PIN_2, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
     strip.begin()
+    strip2.begin()
 
     print('Press Ctrl-C to quit.')
     if not args.clear:
@@ -104,14 +135,14 @@ if __name__ == '__main__':
 
         while True:
             colorWipe(strip, Color(255, 0, 0))  # Red wipe
+            colorWipe(strip2, Color(255, 0, 0))  # Red wipe
             colorWipe(strip, Color(0, 255, 0))  # Green wipe
+            colorWipe(strip2, Color(0, 255, 0))  # Green wipe
             colorWipe(strip, Color(0, 0, 255))  # Blue wipe
-            print('Theater chase animations.')
-            theaterChase(strip, Color(127, 127, 127))  # White theater chase
-            theaterChase(strip, Color(127, 0, 0))  # Red theater chase
-            theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
-            rainbowCycle(strip)
-
+            colorWipe(strip2, Color(0, 0, 255))  # Blue wipe
+            time.sleep(1)
+            strobews1()
+            strobews2()
     except KeyboardInterrupt:
         if args.clear:
             colorWipe(strip, Color(0, 0, 0), 10)
